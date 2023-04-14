bcc675051313b883a0c38dd9695a87dc2f6e29d3
h4ckery.b1nx <51971090+zbinx@users.noreply.github.com>
2023-04-14 12:55:27 -0400

    update controller_fix.sh

--- controller_fix.sh
+++ controller_fix.sh
@@ -8,21 +8,26 @@ echo "Starting script..."
 while read -r line; do
   # Extract the device name from the line if it contains "USB"
   if echo "$line" | grep -q "USB"; then
-    device_name=$(echo "$line" | awk -F'"' '/Name/ {print $2}' | sed 's/ *$//')
-    echo "Processing device: $device_name"
-    # Rename the config file to match the device name
-    old_name=$(find "$config_dir" -type f -name "*.cfg" -iname "*${device_name% }*")
-    if [[ -n "$old_name" ]]; then
-      new_name=$(echo "$old_name" | sed "s/\(.*\)\.cfg/\1\ .cfg/")
-      echo "Renaming file: $old_name to $new_name"
-      mv "$old_name" "$new_name"
-
-      # Update the input_device string in the config file
-      sed -i "s/input_device = \".*\"/input_device = \"$device_name \"/g" "$new_name"
-      
-      echo "Device name added to config file: $new_name"
+    device_name=$(echo "$line" | awk -F'"' '/Name/ {print $2}')
+    if [[ "$device_name" =~ \ $ ]]; then
+      echo "Processing device: $device_name"
+      # Rename the config file to match the device name
+      old_name=$(find "$config_dir" -type f -name "*.cfg" -iname "*${device_name% }*")
+      if [[ -n "$old_name" ]]; then
+        new_name=$(echo "$old_name" | sed "s/\(.*\)\.cfg/\1\ .cfg/")
+        if [[ "$new_name" != "$old_name" ]]; then
+          echo "Renaming file: $old_name to $new_name"
+          mv "$old_name" "$new_name"
+        else
+          echo "Config file already renamed."
+        fi
+        # Replace the input_device string in the config file
+        sed -i "s/input_device = \".*\"/input_device = \"$device_name\"/g" "$new_name"
+      else
+        echo "Config file not found for $device_name"
+      fi
     else
-      echo "Config file not found for $device_name"
+      echo "Skipping device $device_name"
     fi
   fi
 done < "$devices_file"
